f808f048172b95fbe5335543a4bf19c52ea1a9a9
Maelgwapo <mcalimutan_210000000860@uic.edu.ph>
2025-03-21 02:07:24 +0800

    💄 change base route

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/works" element={<Works />} />
           <Route path="/contact" element={<Contact />} />
